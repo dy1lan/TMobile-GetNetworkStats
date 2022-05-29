6a1d210653e98eb566e53985272d400d072d3826
Dylan Stewart <dy1lan@yahoo.com>
2022-05-29 16:17:57 -0400

    - Initial Commit

---
+++ CSV-To-Graph.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import plotly.express as px
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+import os
+
+Tk().withdraw()
+file_path = askopenfilename()
+filename = os.path.basename(file_path)
+if not filename.endswith(".csv"):
+    print("\n\n*******ERROR: File must be a csv produced from GetInternetSpeeds.py.*******")
+    exit()
+
+df = pd.read_csv(file_path)
+
+Four_G_eNBID = df["eNBID"]
+Four_G_Bands = df.columns[2]
+
+Five_G_gNBID = df["gNBID"]
+Five_G_Bands = df.columns[12]
+
+df.drop("eNBID", axis=1, inplace=True)
+df.drop("gNBID", axis=1, inplace=True)
+
+fig = px.line(df, x=df['Time'], y=df.columns[3:8])
+fig2 = px.line(df, x=df["Time"], y=df.columns[12:17])
+
+fig.update_layout(title='4G Stats for {0}'.format(filename),
+                   plot_bgcolor='rgb(230, 230,230)',
+                   showlegend=True)
+fig2.update_layout(title='5G Stats for {0}'.format(filename),
+                   plot_bgcolor='rgb(230, 230,230)',
+                   showlegend=True)
+
+fig.show()
+fig2.show()
+
---
+++ Windows/GetInternetSpeeds.py
@@ -0,0 +1,44 @@
+import json
+import csv
+import os
+import requests 
+import time
+from datetime import date
+from datetime import datetime
+
+while(True):
+    try:
+        today = date.today()
+        now = datetime.now()
+
+        current_time = now.strftime("%H:%M:%S")
+
+        dateToday = today.strftime("%m-%d-%Y")
+
+        filename = "Data\{0}.csv".format(dateToday)
+
+        url = requests.get("http://192.168.12.1/TMI/v1/gateway?get=signal")
+        text = url.text
+
+        data = json.loads(text)
+        
+        signal = data['signal']
+
+        four_g = signal["4g"]
+        five_g = signal["5g"]
+
+        data_file = open(filename, 'a')
+
+        csv_writer = csv.writer(data_file)
+
+        if os.path.getsize(filename) == 0:
+            Headers = ["Time","4G","Bands","Bars","Cell ID(CID)","eNBID","Reference Signal Received Power(RSRP)","Reference Signal Received Quality(RSRQ)","Received Signal Strength Indicator(RSSI)","Signal to Interference & Noise Ratio(SINR)","","5G","Bands","Bars","Cell ID(CID)","gNBID","Reference Signal Received Power(RSRP)","Reference Signal Received Quality(RSRQ)","Received Signal Strength Indicator(RSSI)","Signal to Interference & Noise Ratio(SINR)"]
+            csv_writer.writerow(Headers)
+
+        Values = [current_time, "", "(" + ",".join(four_g['bands']) + ")", four_g['bars'], four_g['cid'], four_g['eNBID'], four_g['rsrp'], four_g['rsrq'], four_g['rssi'], four_g['sinr'], "","", "(" + ",".join(five_g['bands']) + ")", five_g['bars'], five_g['cid'], five_g['gNBID'], five_g['rsrp'], five_g['rsrq'], five_g['rssi'], five_g['sinr']]
+        csv_writer.writerow(Values)
+
+        data_file.close()
+    except:
+        pass
+    time.sleep(300)
